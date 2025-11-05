8d069ae53281670c8e7d8709f962b66f238373d1
Hemnath M <146712643+HEMNATH77@users.noreply.github.com>
2025-11-05 18:56:44 +0530

    Add files via upload

---
+++ DOM/random.js
@@ -0,0 +1,19 @@
+// Random numbers -> printing random numbers
+
+ Syntax : Math.random()  -> prints random numbers from 0 to 1 whenever we cal console.log   
+
+let a = Math.random()
+console.log(a)
+
+// To convert random numbers to whole number  -> Multiply by 10
+
+console.log(Math.random()*10)
+
+// Floor values -> prints the whole number by neglecting the decimals like if "4.653" it prints "4"
+
+ Syntax : Math.floor
+
+console.log(Math.floor(4.6586))               // output : 4
+
+
+
